a7b72d49f8115df465514f5d471bdffddfcc416a
Sen1993 <gsen1993@gmail.com>
2014-05-15 16:51:34 +0800

    learn markdown

--- 3/3.6/1.c
+++ 3/3.6/1.c
@@ -1,24 +1,44 @@
-%{
+/// \file 1.c
+/*
+  ------------------------------------
+    Create date: 2014-05-13 01:38
+    Modified date: 2014-05-15 16:19
+    Author: Sen1993
+    Email: 1730806439@qq.com
+  ------------------------------------
+*/
 
-	#include <stdio.h>
-	int yylval;
+#include <stdio.h>
+#include <string.h>
 
-%}
+void getfail(const char str[], int arr[]){
+	int i;
+	int t = 0;
 
-delim	[ \t]
-ws		{delim}+
-
-%%
-
-
-
-%%
-
-int yywrap(void){
-	return 1;
+	arr[0] = 0;
+	for(i = 1; i < strlen(str); ++i){
+		while(t > 0 && str[i] != str[t]) t = arr[t];
+		if(str[i] == str[t]) arr[i] = ++t;
+		else arr[i] = 0;
+	}
 }
 
 int main(void){
-	yyin = stdin;
-	return yylex();
+	char str[100];
+	int fail[sizeof(str)/sizeof(str[0])];
+	int i;
+
+	printf("input string: ");
+	scanf("%s", str);
+
+	printf(" a[i]: ");
+	for(i = 0; i < strlen(str); ++i)
+		printf("%2c ",str[i]);
+	getfail((const char *)str, fail);
+	printf("\n f(x): ");
+	for(i = 0; i < strlen(str); ++i)
+		printf("%2d ", fail[i]);
+	printf("\n");
+
+	return 0;
 }
